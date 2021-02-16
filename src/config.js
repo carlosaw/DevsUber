a743cc8bc6edd31fe78c88a9e8bce5e3d59927a9
Your Name <you@example.com>
2021-02-16 17:30:24 -0400

    FromLoc toLoc

---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const MapsAPI = 'AIzaSyAajojMkvPJ2SgBDAmW1G1O_qV7jyTRfpM';
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,9 +1,19 @@
-import React, { useRef, useState } from 'react';
-import { StatusBar, SafeAreaView, Text } from 'react-native';
+import React, { useRef, useState, useEffect } from 'react';
+import { StatusBar } from 'react-native';
 import MapView from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
+import Geocoder from 'react-native-geocoding';
+import { MapsAPI } from '../../config';
 
 import { 
-    Container
+    Container,
+    IntineraryArea,
+    IntineraryItem,
+    IntineraryLabel,
+    IntineraryPoint,
+    IntineraryTitle,
+    IntineraryValue,
+    IntineraryPlaceholder
  } from './styled';
 
 const Page = () => {
@@ -11,16 +21,81 @@ const Page = () => {
 
     const [mapLoc, setMapLoc] = useState({
         center:{
-            latitude:-15.656897,
-            longitude:-56.127081
+            //latitude:-15.656897,
+            //longitude:-56.127081
+            latitude:37.78825,
+            longitude:-122.4324
         },
         zoom:16,
         pitch:0,
         altitude:0,
         heading:0
     });
+    const [fromLoc, setFromLoc] = useState({});
+    const [toLoc, setToLoc] = useState({});
+    const [showDirections, setShowDirections] = useState(false);
 
-    return (        
+    useEffect(()=>{
+        Geocoder.init(MapsAPI, {language:'pt-br'});
+        getMyCurrentPosition();
+    }, []);
+    
+    useEffect(()=>{
+        if(fromLoc.center && toLoc.center) {
+            setShowDirections(true);
+        }
+    }, [toLoc]);
+
+    const getMyCurrentPosition = ()  => {
+        Geolocation.getCurrentPosition(async (info)=>{
+
+            const geo = await Geocoder.from(info.coords.latitude, info.coords.longitude);
+
+            if(geo.results.length > 0) {
+                const loc = {
+                    name:geo.results[0].formatted_address,
+                    center:{
+                        latitude:info.coords.latitude,
+                        longitude:info.coords.longitude
+                    },
+                    zoom:16,
+                    pitch:0,
+                    altitude:0,
+                    heading:0
+                };
+                setMapLoc(loc);
+                setFromLoc(loc);
+
+            }
+
+        }, (error)=>{
+
+        });
+    }
+
+    const handleFromClick = () => {
+
+    }
+    const handleToClick = async () => {
+        const geo = await Geocoder.from('Santo Antonio do Leverger, MT');
+        if(geo.results.length > 0) {
+            const loc = {
+                name:geo.results[0].formatted_address,
+                center:{
+                    latitude:geo.results[0].geometry.location.lat,
+                    longitude:geo.results[0].geometry.location.lng
+                },
+                zoom:16,
+                pitch:0,
+                altitude:0,
+                heading:0
+            };
+            setToLoc(loc);
+        }
+    }
+
+    return (
+                
         <Container>
             <StatusBar barStyle="light-content" />
             <MapView
@@ -28,8 +103,52 @@ const Page = () => {
                 style={{flex:1}}
                 provider="google"
                 camera={mapLoc}
-            ></MapView>
-        </Container>            
+            >
+            
+                {fromLoc.center &&
+                    <MapView.Marker pinColor="black" coordinate={fromLoc.center} />
+                }
+                {toLoc.center &&
+                    <MapView.Marker pinColor="black" coordinate={toLoc.center} />
+                }
+
+                {showDirections &&
+                    <></>
+                }
+
+            </MapView>
+            <IntineraryArea>
+                <IntineraryItem onPress={handleFromClick} underlayColor="#EEE">
+                <>
+                    <IntineraryLabel>
+                        <IntineraryPoint color="#0000FF" />
+                        <IntineraryTitle>Origem</IntineraryTitle>
+                    </IntineraryLabel>
+                    {fromLoc.name &&
+                        <IntineraryValue>{fromLoc.name}</IntineraryValue>
+                    }
+                    {!fromLoc.name &&
+                        <IntineraryPlaceholder>Escolha um local de origem</IntineraryPlaceholder>
+                    }
+                </>
+                </IntineraryItem>
+                <IntineraryItem onPress={handleToClick} underlayColor="#EEE">
+                <>
+                    <IntineraryLabel>
+                        <IntineraryPoint color="#00FF00" />
+                        <IntineraryTitle>Destino</IntineraryTitle>
+                    </IntineraryLabel>
+                    {toLoc.name &&
+                        <IntineraryValue>{toLoc.name}</IntineraryValue>
+                    }
+                    {!toLoc.name &&
+                        <IntineraryPlaceholder>Escolha um local de destino</IntineraryPlaceholder>
+                    }
+                </>
+                </IntineraryItem>
+            </IntineraryArea>
+        </Container>
+                    
     );
 }
 
