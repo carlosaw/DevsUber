b385e95097a92defd78d188110c3d109dc8aa080
unknown <carlosfreevg@gmail.com>
2021-02-19 21:25:01 -0400

    Trip

--- src/components/DriverModal.js
+++ src/components/DriverModal.js
@@ -9,21 +9,62 @@ const ModalArea = styled.View`
     align-items: center;
 `;
 
+const DriverHeadline = styled.Text`
+    font-size: 18px;
+    font-weight: bold;
+    color: #000;
+    margin-bottom: 20px;
+`;
 const DriverAvatar = styled.Image`
   width: 120px;
   height: 120px;
   border-radius: 60px;
 `;
+const DriverName = styled.Text`
+    margin: 20px;
+    font-size: 18px;
+    font-weight: bold;
+    color: #000;
+`;
+const DriverStars = styled.Text`
+    color: #999;
+    font-size: 17px;
+`;
+const DriveCarInfo = styled.View`
+    width: 100%;
+    margin: 20px;
+    border-top-width: 1px;
+    border-top-color: #999;
+    border-bottom-width: 1px;
+    border-bottom-color: #999;
+    align-items: center;
+    padding: 20px;
+`;
+const DriverCar = styled.Text`
+    font-size: 17px;
+    color: #000;
+`;
+const DriverColor = styled.Text`
+    font-size: 15px;
+    color: #999;
+`;
+const DriverPlate = styled.Text`
+    font-size: 20px;
+    color: #000;
+`;
 
-const DriverName = styled.Text``;
-
-const DriverStars = styled.Text``;
-
-const DriverCar = styled.Text``;
-
-const DriverColor = styled.Text``;
-
-const DriverPlate = styled. Text``;
+const TripButton = styled.TouchableHighlight`
+    height: 50px;
+    background-color: #3574CB;
+    border-radius: 5px;
+    justify-content: center;
+    align-items: center;
+    width: 80%;
+`;
+const TripButtonText = styled.Text`
+    font-size: 17px;
+    color: #FFF;
+`;
 
 
 export default (props) => {
@@ -36,12 +77,18 @@ export default (props) => {
             visible={props.visible}
         >
             <ModalArea>
+                <DriverHeadline>Seu motorista é:</DriverHeadline>
                 <DriverAvatar source={{uri:props.driver.avatar}} />
                 <DriverName>{props.driver.name}</DriverName>
-                <DriverStars>{props.driver.stars}</DriverStars>
-                <DriverCar>{props.driver.carName}</DriverCar>
-                <DriverColor>{props.driver.carColor}</DriverColor>
-                <DriverPlate>{props.driver.carPlate}</DriverPlate>
+                <DriverStars>{props.driver.stars} estrelas</DriverStars>
+                <DriveCarInfo>
+                    <DriverCar>{props.driver.carName}</DriverCar>
+                    <DriverColor>{props.driver.carColor}</DriverColor>
+                    <DriverPlate>{props.driver.carPlate}</DriverPlate>
+                </DriveCarInfo>
+                    <TripButton>
+                        <TripButtonText>Encerrar Viagem</TripButtonText>
+                    </TripButton>
             </ModalArea>            
         </Modal>
     );
